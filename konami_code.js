902a136b6006e32365deeba4f772ecdb5834caec
Julianne Kim <juliannne@gmail.com>
2017-10-03 18:09:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,8 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
 
-  document.addEventListener('keydown', function(e) {
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
